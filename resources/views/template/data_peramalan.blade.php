c53c1dd3ad20a9c654580deb3a13ccd714f543df
M Nur Alfiansyah <mnuralfiansyah@gmail.com>
2018-08-02 17:00:02 +0800

    bismillah fix lagi

--- resources/views/template/data_peramalan.blade.php
+++ resources/views/template/data_peramalan.blade.php
@@ -87,28 +87,28 @@
 <!-- /#wrapper -->
 
 <!-- jQuery -->
-<script src="{{asset('template/vendor/jquery/jquery.min.js')}}"></script>
+<script src="{{secure_asset('template/vendor/jquery/jquery.min.js')}}"></script>
 <!-- Bootstrap Core JavaScript -->
-<script src="{{asset('template/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+<script src="{{secure_asset('template/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
 
 <!-- Metis Menu Plugin JavaScript -->
-<script src="{{asset('template/vendor/metisMenu/metisMenu.min.js')}}"></script>
+<script src="{{secure_asset('template/vendor/metisMenu/metisMenu.min.js')}}"></script>
 
 <!-- Morris Charts JavaScript -->
-<script src="{{asset('template/vendor/raphael/raphael.min.js')}}"></script>
-<script src="{{asset('template/vendor/morrisjs/morris.min.js')}}"></script>
-<script src="{{asset('template/data/morris-data.js')}}"></script>
+<script src="{{secure_asset('template/vendor/raphael/raphael.min.js')}}"></script>
+<script src="{{secure_asset('template/vendor/morrisjs/morris.min.js')}}"></script>
+<script src="{{secure_asset('template/data/morris-data.js')}}"></script>
 
 <!-- Custom Theme JavaScript -->
-<script src="{{asset('template/dist/js/sb-admin-2.js')}}"></script>
+<script src="{{secure_asset('template/dist/js/sb-admin-2.js')}}"></script>
 
 <!-- DataTables JavaScript -->
-<script src="{{asset('template/vendor/datatables/js/jquery.dataTables.min.js')}}"></script>
-<script src="{{asset('template/vendor/datatables-plugins/dataTables.bootstrap.min.js')}}"></script>
-<script src="{{asset('template/vendor/datatables-responsive/dataTables.responsive.js')}}"></script>
+<script src="{{secure_asset('template/vendor/datatables/js/jquery.dataTables.min.js')}}"></script>
+<script src="{{secure_asset('template/vendor/datatables-plugins/dataTables.bootstrap.min.js')}}"></script>
+<script src="{{secure_asset('template/vendor/datatables-responsive/dataTables.responsive.js')}}"></script>
 
-<script src="{{asset('template/vendor/chartist/chartist.min.js')}}"></script>
-<script src="{{asset('template/vendor/chartist/chartist-plugin-tooltip.min.js')}}"></script>
+<script src="{{secure_asset('template/vendor/chartist/chartist.min.js')}}"></script>
+<script src="{{secure_asset('template/vendor/chartist/chartist-plugin-tooltip.min.js')}}"></script>
 
 <script>
 $(document).ready(function() {
